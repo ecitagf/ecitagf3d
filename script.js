e82139a9944f7683626126e5219592c9383ed38d
ecitagf <110501344+ecitagf@users.noreply.github.com>
2022-08-05 12:37:06 -0300

    Commit
    Segundo commit

--- script.js
+++ script.js
@@ -1,13 +1,69 @@
 import {GLTFLoader} from "./GLTFLoader.js";
-import {OrbitControls} from "./OrbitControls.js"
+import {OrbitControls} from "./OrbitControls.js";
+import {Sky} from "./Sky.js";
+import {GUI} from "./lil-gui.module.min.js"
 		    
+		    function initSky() {
+		    //adicionar céu
+		        
+		        var sky = new Sky ();
+		        sky.scale.setScalar ( 450000 );
+		        scene.add( sky );
+		        
+		        var sun = new THREE.Vector3();
+		        
+		        const effectController = {
+		            
+		            turbidity : 10,
+		            rayleigh : 3,
+		            mieCoefficient : 0.005,
+		            mieDirectionalG : 0.7,
+		            elevation : 2,
+		            azimuth : 180,
+		            exposure: renderer.toneMappingExposure
+		        };
+		        
+		        function guiChanged() {
+		            
+		        const uniforms = sky.material.uniforms;
+					uniforms[ 'turbidity' ].value = effectController.turbidity;
+					uniforms[ 'rayleigh' ].value = effectController.rayleigh;
+					uniforms[ 'mieCoefficient' ].value = effectController.mieCoefficient;
+					uniforms[ 'mieDirectionalG' ].value = effectController.mieDirectionalG;
+					
+					const phi = THREE.MathUtils.degToRad( 90 - effectController.elevation );
+					const theta = THREE.MathUtils.degToRad( effectController.azimuth );
+					
+					sun.setFromSphericalCoords( 1, phi, theta );
+					
+					uniforms[ 'sunPosition' ].value.copy( sun );
+					
+					renderer.toneMappingExposure = 					effectController.exposure;
+					renderer.render( scene, camera );
+
+
+		        }
+		        
+		        const gui = new GUI();
+		        
+		        gui.add( effectController, 'turbidity', 0.0, 20.0, 0.1 ).onChange( guiChanged );
+				gui.add( effectController, 'rayleigh', 0.0, 4, 0.001 ).onChange( guiChanged );
+				gui.add( effectController, 'mieCoefficient', 0.0, 0.1, 0.001 ).onChange( guiChanged );
+				gui.add( effectController, 'mieDirectionalG', 0.0, 1, 0.001 ).onChange( guiChanged );
+				gui.add( effectController, 'elevation', 0, 90, 0.1 ).onChange( guiChanged );
+				gui.add( effectController, 'azimuth', - 180, 180, 0.1 ).onChange( guiChanged );
+				gui.add( effectController, 'exposure', 0, 1, 0.0001 ).onChange( guiChanged );
+				
+				guiChanged();
+
+		    }
 			var scene = new THREE.Scene();
 			scene.background = new THREE.Color(0xdddddd);
 			
 			scene.add(new THREE.AxesHelper(500));
 			
 			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-			camera.position.set(30, 60, 30);
+			camera.position.set(-100, 60, -10);
 			
 			var hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
 			scene.add(hemiLight);
@@ -28,6 +84,8 @@ import {OrbitControls} from "./OrbitControls.js"
 			
 			var controls = new OrbitControls( camera, renderer.domElement );
 			controls.enableDamping = true;
+			
+			initSky();
 
 			var loader = new GLTFLoader();
 			
@@ -38,7 +96,7 @@ import {OrbitControls} from "./OrbitControls.js"
 			    if (n.isMesh) {
 			        n.castShadow = true;
 			        n.receiveShadow = true;
-			        if (n.material.map) n.material.map.anisotropy = 16;
+			        if (n.material.map) n.material.map.anisotropy = 8;
 			    }
 			});
 			scene.add(gltf.scene);
@@ -55,4 +113,4 @@ import {OrbitControls} from "./OrbitControls.js"
 				)
 			};
 
-			animate();e();
+			animate();
